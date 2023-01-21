0b81b91dbe4f09b51987a8d49152fd37571bdcba
Fran Zekan <zekan.fran369@gmail.com>
2023-01-21 19:53:42 +0100

    Add how to run failed tests

--- ppj-lab4/test.mjs
+++ ppj-lab4/test.mjs
@@ -8,6 +8,9 @@ import { readdir } from "node:fs/promises";
 import { join } from "node:path";
 
 let passed = 0;
+const failed = [];
+const errored = [];
+
 const files = await readdir(join(__dirname, "./tests"));
 for (const file of files) {
   console.log(`----- Testing ${file} -----`);
@@ -16,6 +19,7 @@ for (const file of files) {
     await $`python3 ./FRISCGenerator.py ./tests/${file}/a.frisc < ./tests/${file}/test.in`;
   } catch (err) {
     console.log(`Failed to generate frisc code`);
+    errored.push(file);
     continue;
   }
 
@@ -27,8 +31,17 @@ for (const file of files) {
     passed++;
   } catch (error) {
     console.log(`Test ${file} failed`);
+    failed.push(file);
   }
 }
 
 console.log("");
 console.log(`Passed ${passed} / ${files.length} tests`);
+
+errored.forEach((file) => {
+  console.log(`Compiler errored [${file}] -> \nnr compiler:watch ./tests/${file}/test.in`);
+});
+
+failed.forEach((file) => {
+  console.log(`Runtime wrong [${file}] -> \nnr compiler:watch ./tests/${file}/test.in`);
+});
